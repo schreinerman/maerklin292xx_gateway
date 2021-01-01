a61de9819bca400888e31a973edc894342333077
schreinerman <manuel@schreiner-home.info>
2021-01-01 19:42:18 +0100

    Add files via upload

---
+++ maerklin29210_gateway.ino
@@ -0,0 +1,274 @@
+
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char *ssid = "ssid";
+const char *password = "password";
+
+WebServer server(80);
+
+
+typedef enum en_maerklin_ir_remote_address
+{
+  enMaerklinIrRemoteAddressC = 0x5,
+  enMaerklinIrRemoteAddressD = 0xa,
+} en_maerklin_ir_remote_address_t;
+
+
+typedef enum en_maerklin_ir_remote_func
+{
+  enMaerklinIrRemoteFuncStop = 0,
+  enMaerklinIrRemoteFuncBackward = 1,
+  enMaerklinIrRemoteFuncForward = 2,
+  enMaerklinIrRemoteFuncSound1 = 3,
+  enMaerklinIrRemoteFuncSoundDoorClose = 3,
+  enMaerklinIrRemoteFuncSoundCoupler = 3,
+  enMaerklinIrRemoteFuncSound2 = 4,
+  enMaerklinIrRemoteFuncSoundHorn = 4,
+  enMaerklinIrRemoteFuncSound3 = 5,
+  enMaerklinIrRemoteFuncSoundMotor = 5,
+  enMaerklinIrRemoteFuncSoundAnnouncement = 5,
+  enMaerklinIrRemoteFuncLight = 6,
+} en_maerklin_ir_remote_func_t;
+
+const uint16_t kIrLed = 12;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+
+uint16_t codeCache[17];
+
+const char indexHtml[] = "<html>\r\n"
+"<head>\r\n"
+"    <!-- meta http-equiv='refresh' content='5'/-->\r\n"
+"    <meta name=\"mobile-web-app-capable\" content=\"yes\">\r\n"
+"    <meta charset=\"utf-8\">\r\n"
+"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
+"    <meta name=\"apple-mobile-web-app-capable\" content=\"yes\" />\r\n"
+"    <meta name=\"apple-mobile-web-app-status-bar-style\" content=\"black\" />\r\n"
+"    <meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\">\r\n"
+"    <title>Maerklin IR Remote</title>\r\n"
+"    <script>\r\n"
+"    function execcmd(cmd) {\r\n"
+"        fetch('/cmd/' + cmd);\r\n"
+"    }\r\n"
+"    function requestFullScreen() {\r\n"
+"        const _element = document.documentElement;\r\n"
+"        if (_element.requestFullscreen) {\r\n"
+"           _element.requestFullscreen();\r\n"
+"        } else {\r\n"
+"        if (_element.mozRequestFullScreen) {\r\n"
+"           _element.mozRequestFullScreen();\r\n"
+"        } else {\r\n"
+"           if (_element.webkitRequestFullscreen) {\r\n"
+"              _element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);\r\n"
+"           }\r\n"
+"        }\r\n"
+"      }\r\n"
+"    }\r\n"
+"    function blockMove() {\r\n"
+"          event.preventDefault() ;\r\n"
+"    }\r\n"
+"    window.setTimeout(function() {\r\n"
+"        requestFullScreen();\r\n"
+"    },1000);\r\n"
+"    requestFullScreen();\r\n"
+"    </script>\r\n"
+"    <style>\r\n"
+"    body {\r\n"
+"        background-color: #000000;\r\n"
+"        font-family: Arial, Helvetica, Sans-Serif;\r\n"
+"        Color: #000088;\r\n"
+"    }\r\n"
+"    \r\n"
+"    td {\r\n"
+"        background-color: #ccccff;\r\n"
+"    }"
+"    \r\n"
+"    input, button {\r\n"
+"        width: 15%;\r\n"
+"        height: 10%;\r\n"
+"        box-sizing: border-box;\r\n"
+"        margin: 0.5rem auto;\r\n"
+"        padding: 0.75rem;\r\n"
+"        font-size: 0.75rem;\r\n"
+"        text-align: center;\r\n"
+"        text-transform: uppercase;\r\n"
+"        color: white;\r\n"
+"        border: none;\r\n"
+"        background: #4e4eff;\r\n"
+"        border-radius: 0.5rem;\r\n"
+"        outline: none;\r\n"
+"        user-select: none;\r\n"
+"        cursor: pointer;\r\n"
+"    }\r\n"
+"    </style>\r\n"
+"</head>\r\n"
+"<body ontouchmove=\"blockMove()\">\r\n"
+"        <div style=\"height:35%\"></div>\r\n"
+"        <center>\r\n"
+"            <p>\r\n"
+"                <button onclick=\"execcmd('light')\" style=\"width:24%\">Licht</button>\r\n" 
+"                <button onclick=\"execcmd('sound/1')\" style=\"width:24%\">Motor</button>\r\n"
+"                <button onclick=\"execcmd('sound/2')\" style=\"width:24%\">Horn</button>\r\n"
+"                <button onclick=\"execcmd('sound/3')\" style=\"width:24%\">Kupplung</button>\r\n"
+"            </p>\r\n"
+"            <p>\r\n"
+"                <button onclick=\"execcmd('speed/-3')\">&lt;</button>\r\n"  
+"                <button style=\"background-color: #8e8eff; color: #000000;\" onclick=\"execcmd('speed/-2')\">&lt;</button>\r\n"  
+"                <button style=\"background-color: #ceceff; color: #000000;\" onclick=\"execcmd('speed/-1')\">&lt;</button>\r\n"  
+"                <button style=\"background-color: #ceceff; color: #000000;\" onclick=\"execcmd('speed/1')\">&gt;</button>\r\n"  
+"                <button style=\"background-color: #8e8eff; color: #000000;\" onclick=\"execcmd('speed/2')\">&gt;</button>\r\n"  
+"                <button onclick=\"execcmd('speed/3')\">&gt;</button>\r\n"
+"            </p>\r\n"
+"            <p> \r\n"
+"                <button onclick=\"execcmd('speed/0')\" style=\"width:50%\">STOP</button>\r\n"  
+"            </p>\r\n"
+"        </center>\r\n"
+"    </div>\r\n"
+"</body>\r\n"
+"</html>\r\n";
+
+
+
+void handleRoot() {
+  server.send(200, "text/html", (char*)indexHtml);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //intiate serial port
+  Serial.begin(115200);
+
+  //initiate IR library
+  irsend.begin();
+
+  //initiate WIFI
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/cmd/{}/{}", []() {
+    String cmd = server.pathArg(0);
+    String cmdArg = server.pathArg(1);
+    if (cmd == "sound")
+    {
+        if ((cmdArg == "motor") || (cmdArg == "3"))
+        {
+            sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncSound1);
+        } else if ((cmdArg == "horn") || (cmdArg == "2"))
+        {
+            sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncSound2);
+        } else if ((cmdArg == "coupler") || (cmdArg == "1"))
+        {
+            sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncSound3);
+        } 
+    } else if (cmd == "speed")
+    {
+      int speed = cmdArg.toInt();
+      sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncStop);
+      if (speed > 0)
+      {
+         while(speed != 0)
+         {
+            speed--;
+            sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncForward);
+         }
+      } else if (speed < 0)
+      {
+         while(speed != 0)
+         {
+            speed++;
+            sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncBackward);
+         }
+      } 
+    }
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.on("/cmd/{}", []() {
+    String cmd = server.pathArg(0);
+    if (cmd == "forward")
+    {
+        sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncForward);
+    } else if (cmd == "stop")
+    {
+        sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncStop);
+    }  else if (cmd == "backward")
+    {
+        sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncBackward);
+    } else if (cmd == "light")
+    {
+        sendMaerklin(enMaerklinIrRemoteAddressC,enMaerklinIrRemoteFuncLight);
+    }
+    server.send(200, "text/plain", "this works as well");
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+  
+}
+
+void sendMaerklin(en_maerklin_ir_remote_address_t enAddress, en_maerklin_ir_remote_func_t enFunction)
+{
+  static bool bToggle = false;
+  static uint8_t u8LastCommand = 0;
+  uint8_t u8Command = (((uint8_t)enFunction & 0x7) << 4) | ((uint8_t)enAddress & 0xf);
+  int i;
+  if (u8Command != u8LastCommand)
+  {
+    bToggle = false;
+  }
+  u8LastCommand = u8Command;
+  
+  if (bToggle)
+  {
+    u8Command |= 0x80;
+  }
+  codeCache[0] = 5400;
+  
+  for(i = 0;i < 8;i++)
+  {
+    if ((u8Command & 0x80) != 0) 
+    {
+      codeCache[i*2 + 1] = 1700;
+      codeCache[i*2 + 2] = 600;
+    } else {
+      codeCache[i*2 + 1] = 600;
+      codeCache[i*2 + 2] = 1700;
+    }
+    u8Command = u8Command << 1;
+  }
+  irsend.sendRaw(codeCache,17,38);
+  delay(10);
+  bToggle = !bToggle;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  server.handleClient();
+}
